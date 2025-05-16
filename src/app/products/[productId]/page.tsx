65394c330ac7b5a87497055e796677fa13bab347
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2025-05-16 13:55:25 +0330

    implement link navigation

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -12,14 +12,21 @@ export async function generateMetadata(
 ): Promise<Metadata> {
   const productId = (await params).productId;
 
-  const product = await fetch(
-    `https://fakestoreapi.com/products/${productId}`
-  ).then((res) => res.json());
+  try {
+    const res = await fetch(`https://fakestoreapi.com/products/${productId}`);
+    const product = await res.json();
 
-  return {
-    title: product?.title,
-    description: product?.description,
-  };
+    return {
+      title: product?.title,
+      description: product?.description,
+    };
+  }
+  catch (error) {
+    return {
+      title: 'no data found',
+      description: 'empty'
+    }
+  }
 }
 
 interface PropsType {
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+
+const views = [
+  { id: 1, viewNumber: 1, viewName: "review 1" },
+  { id: 2, viewNumber: 2, viewName: "review 2" },
+];
 
 const Products = () => {
   return (
-    <div>Products</div>
-  )
-}
+    <div>
+      {views.map((view) => (
+        <li key={view.id}>
+          <Link
+            href={`/products/1/reviews/${view.viewNumber}`}
+            replace
+            prefetch
+            scroll
+          >
+            {view.viewName}
+          </Link>
+        </li>
+      ))}
+    </div>
+  );
+};
 
-export default Products
+export default Products;
