a1f6c20d9726f9e3fa372e118fa6fdb9de10639f
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2025-05-16 16:34:53 +0330

    use link, useRouter and usePathname

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,51 +1,30 @@
+"use client";
 import Link from "next/link";
-import React, { Suspense } from "react";
-import Loading from "@/app/products/loading";
+import React from "react";
+import { usePathname } from "next/navigation";
+
+const products = [
+    { id: 1, name: "lap top" },
+    { id: 2, name: "head phone" },
+    { id: 3, name: "phone" },
+    { id: 4, name: "smart watch" },
+];
 
 const Products = () => {
+    const pathname = usePathname();
+
     return (
         <div>
-            <h1>All products</h1>
-            <Suspense fallback={<Loading />}>
-                <ProductList />
-            </Suspense>
+            {products.map((product) => {
+                return (
+                    <li key={product.id}>
+                        <Link href={`/products/${product.id}`}>
+                            {product.name}
+                        </Link>
+                    </li>
+                );
+            })}
         </div>
-    );
-};
-
-const ProductList = async (): Promise<React.ReactNode> => {
-    let data: { id: number; title: string }[] | null = null;
-    let error = null;
-
-    try {
-      const res = await fetch(`https://fakestoreapi.com/products`);
-
-      if (!res.ok) return <p>An error occured in fetching data...</p>;
-
-      data= await res.json();
-    }
-    catch (err) {
-      error = err;
-    }
-
-    if (data === null) return <p>no data found</p>
-    if (error) return <p>you have error</p>
-
-    return (
-        <React.Fragment>
-            {data.map((product) => (
-                <li key={product.id}>
-                    <Link
-                        href={`/products/${product?.id}`}
-                        replace={false}
-                        prefetch
-                        scroll
-                    >
-                        {product?.title}
-                    </Link>
-                </li>
-            ))}
-        </React.Fragment>
     );
 };
 
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,7 +1,12 @@
+'use client'
 import React from "react";
+import { useRouter } from "next/navigation";
 
 const User: React.FC = (): React.JSX.Element => {
-  return <div>user</div>;
+  const { push , replace , refresh } = useRouter();
+  return <div>
+    <button onClick={() => push("/products")}>go to products</button>
+  </div>;
 };
 
 export default User;
